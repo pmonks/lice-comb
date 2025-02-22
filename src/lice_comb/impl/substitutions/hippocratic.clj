b4499303973d5f3475b36ec79fd27f23a888fd4a
Peter Monks <pmonks@gmail.com>
2025-02-21 18:43:20 -0800

    :construction: WIP on replace-first algorithm - case insensitivity tests

--- src/lice_comb/impl/substitutions/hippocratic.clj
+++ src/lice_comb/impl/substitutions/hippocratic.clj
@@ -23,7 +23,7 @@
 
 (def ^:private pairs-d (delay (concat
   (lcisu/spdx-match-pairs @ids-d)                                                                     ; Generic license regexes handle most cases, except...
-  [[(re/join #"Hippocratic([\s\-–—]+Licen?[cs]e)?" (re/opt-grp lcir/fre-ows lcir/fre-only-or-later))  ; ...when no version is provided
+  [[(re/join #"(?i)Hippocratic([\s\-–—]+Licen?[cs]e)?" (re/opt-grp lcir/fre-ows lcir/fre-only-or-later))  ; ...when no version is provided
    (fn [m]
      {:id                      (str "Hippocratic-2.1" (when (get m "orLater") "+"))
       :type                    :concluded
